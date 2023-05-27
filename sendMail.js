c924d76000b9bd39923a61c499d965b5787f0b73
Thales Morais de Almeida <thalesmoraisdealmeida@pop-os.localdomain>
2023-05-27 16:28:13 -0300

    test

--- sendMail.js
+++ sendMail.js
@@ -1,33 +1,13 @@
+import { faker } from '@faker-js/faker';
+
 
 async function sendMail() {
-  const data = JSON.stringify({
-    "FromEmail": "thales.morais21@gmail.com",
-    "FromName": "Your Mailjet Pilot",
-    "Recipients": [
-      {
-        "Email": "thales.morais21@gmail.com",
-        "Name": "Thales Morais"
-      }
-    ],
-    "Subject": "Your email flight plan!",
-    "Text-part": "Dear passenger, welcome to Mailjet! May the delivery force be with you!",
-    "Html-part": "<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you!"
-  });
-  
-  const xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-  
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
-    }
-  });
-  
-  xhr.open("POST", "https://api.mailjet.com/v3/send");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("Authorization", "Basic YTBkMjMzNzM5MjIzOTg4NzVjNDI4OGY2YTAyMzc1YTM6NDU1ZGUyMDYwMDk5Y2JiZjZkOTIxM2JlYjVlNzZjNDc=");
-  
-  const response = xhr.send(data);
 
-  console.log(response)
+// or, if desiring a different locale
+// import { fakerDE as faker } from '@faker-js/faker';
+
+const randomName = faker.person.fullName(); // Rowan Nikolaus
+const randomEmail = faker.internet.email();
+
+console.log(randomName)
 }
