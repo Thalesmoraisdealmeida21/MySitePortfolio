36f27dae64e66abbb523940285069dc1088dd84e
Thales Morais de Almeida <thalesmoraisdealmeida@pop-os.localdomain>
2023-05-27 20:54:13 -0300

    Change email system

--- sendMail.js
+++ sendMail.js
@@ -1,13 +1,41 @@
-import { faker } from '@faker-js/faker';
-
 
 async function sendMail() {
+  const nome = document.getElementById('name').value
+  const email = document.getElementById('email').value
+  const assunto = document.getElementById('subject').value
 
-// or, if desiring a different locale
-// import { fakerDE as faker } from '@faker-js/faker';
 
-const randomName = faker.person.fullName(); // Rowan Nikolaus
-const randomEmail = faker.internet.email();
+  document.getElementById('inputsForm').style.display = 'none'
+  document.getElementById('spinnerLoader').style.display = 'flex'
+  document.getElementById('spanText').style.display = 'block'
 
-console.log(randomName)
+
+  const data = {
+    'subject': assunto,
+    'text': 'Contato',
+    'html': `
+      <h1>Nome: </h1> ${nome} <br />
+      <h1>Email: </h1> ${email} <br />
+      <h1>assunto: </h1> ${assunto} 
+    `,
+    'from': 'Contato 👻 <contato@thalesmorais.dev>'
+  }
+
+  const respon = await fetch('https://mail-app-wqj8.onrender.com/sendmail/thales.morais21@gmail.com', {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'default',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+
+  console.log(JSON.stringify(data))
+
+  if(respon.body.status = 'ok') {
+    document.getElementById('check').style.display = 'block'
+    document.getElementById('spinnerLoader').style.display = 'none'
+    document.getElementById('spanText').innerHTML = "Contato recebido com sucesso"
+  }
 }
